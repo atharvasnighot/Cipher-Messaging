514ae64f27c3d715d7528f155aedecb52fdb4388
Kamesh <kameshrg275@gmai.com>
2023-11-29 20:05:52 +0530

    Added the functionality for viewing the story

--- Frontend/src/components/Status/DummyStory.jsx
+++ Frontend/src/components/Status/DummyStory.jsx
@@ -1,12 +1,12 @@
 export const stories=[
     {
-        image:"cipher.png"
+        image:"https://images.pexels.com/photos/4835419/pexels-photo-4835419.jpeg?auto=compress&cs=tinysrgb&w=600"
     },
     {
-        image:"nigga.png"
+        image:"https://images.pexels.com/photos/15226445/pexels-photo-15226445/free-photo-of-a-sunset-over-a-city-with-clouds-in-the-sky.jpeg?auto=compress&cs=tinysrgb&w=600"
     },
-    {
-        image:"luffy.jpeg"
-    },
+    // {
+    //     image:"luffy.jpeg"
+    // },
 ]
-
+ 
--- Frontend/src/components/Status/StatusViewer.jsx
+++ Frontend/src/components/Status/StatusViewer.jsx
@@ -1,17 +1,50 @@
-
-import { useState } from 'react';
-import { stories } from './DummyStory';
+import { useEffect, useState } from "react";
+import { stories } from "./DummyStory";
+import ProgressBar from "./ProgressBar";
 const StatusViewer = () => {
-    const [currentStoryIndex,setCurrentStoryIndex] =useState(0);
+  const [currentStoryIndex, setCurrentStoryIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleNextStory = () => {
+    if (currentStoryIndex < stories?.length - 1) {
+      setCurrentStoryIndex(currentStoryIndex + 1);
+      setActiveIndex(activeIndex+1);
+    } else {
+      setCurrentStoryIndex(0);
+      setActiveIndex(0);
+    }
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      handleNextStory();
+    },2000);
+    return () => clearInterval(intervalId);
+  }, [currentStoryIndex]);
+
   return (
     <div>
-        <div className='flex justify-center items-center h-[100vh] bg-slate-900'>
-            <div className='relative'> 
-                <img className='max-h-[96vh] object-contain' src={stories?.[currentStoryIndex].image} alt="" />
-            </div>
+      <div className="flex justify-center items-center h-[100vh] bg-slate-900">
+        <div className="relative">
+          <img
+            className="max-h-[96vh] object-contain"
+            src={stories?.[currentStoryIndex].image}
+            alt=""
+          />
+          <div className="absolute top-0 flex w-full">
+            {stories.map((item, index) => (
+              <ProgressBar
+                key={index}
+                duration={2000}
+                index={index}
+                activeIndex={activeIndex}
+              />
+            ))}
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StatusViewer
+export default StatusViewer;
