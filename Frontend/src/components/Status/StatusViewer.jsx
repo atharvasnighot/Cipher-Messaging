dd063c972e8e6ca6ab2bf8a2c977c7eb40bb1cba
Kamesh <kameshrg275@gmail.com>
2023-11-30 11:36:43 +0530

    Added Functionality to StatusViewer

--- Frontend/src/components/Status/StatusViewer.jsx
+++ Frontend/src/components/Status/StatusViewer.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import { stories } from "./DummyStory";
 import ProgressBar from "./ProgressBar";
+import { BsArrowLeft } from "react-icons/bs";
+import { AiOutlineClose } from "react-icons/ai";
+import { Navigate, useNavigate } from "react-router-dom";
 const StatusViewer = () => {
   const [currentStoryIndex, setCurrentStoryIndex] = useState(0);
   const [activeIndex, setActiveIndex] = useState(0);
+  const navigate=useNavigate();
 
   const handleNextStory = () => {
     if (currentStoryIndex < stories?.length - 1) {
@@ -22,9 +26,13 @@ const StatusViewer = () => {
     return () => clearInterval(intervalId);
   }, [currentStoryIndex]);
 
+  const handleNavigate = ()=>{
+    navigate(-1);
+  }
+
   return (
     <div>
-      <div className="flex justify-center items-center h-[100vh] bg-slate-900">
+      <div className= "relative flex justify-center items-center h-[100vh] bg-slate-900">
         <div className="relative">
           <img
             className="max-h-[96vh] object-contain"
@@ -41,6 +49,10 @@ const StatusViewer = () => {
               />
             ))}
           </div>
+        </div>
+        <div>
+          <BsArrowLeft onClick={handleNavigate} className="text-white text-2xl cursor-pointer absolute top-5 left-10"/>
+          <AiOutlineClose onClick={handleNavigate} className="text-white text-2xl cursor-pointer absolute top-4 right-10"/>
         </div>
       </div>
     </div>
