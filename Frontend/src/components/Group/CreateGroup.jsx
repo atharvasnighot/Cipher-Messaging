a9b4c03d258ac2815cbfc0e49bb357494bec99cd
Kamesh <kameshrg275@gmail.com>
2023-12-02 17:26:10 +0530

    Optimized the Crete Group and NewGroup Styling

--- Frontend/src/components/Group/CreateGroup.jsx
+++ Frontend/src/components/Group/CreateGroup.jsx
@@ -23,64 +23,67 @@ const CreateGroup = () => {
   return (
     <div className="w-full h-full ">
       {!newGroup && (
-        <div>
-          <div className="flex items-center space-x-10 bg-[#3e7bd2] text-white pt-16 px-10 pb-5">
-            <BsArrowLeft
+        <>
+          <div className="relative">
+            <div
               onClick={handleNavigate}
-              className="cursor-pointer text-2xl font-bold"
-            />
-            <p className="text-xl font-semibold">Add Group Participant</p>
-          </div>
+              className="flex items-center space-x-10 bg-[#3e7bd2] text-white pt-16 px-10 pb-5 "
+            >
+              <BsArrowLeft className="cursor-pointer text-2xl font-bold" />
+              <p className="text-xl font-semibold">Add Group Participant</p>
+            </div>
 
-          <div className="relative bg-white py-4 px-3">
-            <div className="flex space-x-2 flex-wrap space-y-1">
-              {groupMember.size > 0 &&
-                Array.from(groupMember).map((item) => (
-                  <SelectedMember
-                    handleRemoveMember={() => handleRemoveMember(item)}
-                    member={item}
-                  />
+            <div className="relative bg-white py-4 px-3">
+              <div className="flex space-x-2 flex-wrap space-y-1">
+                {groupMember.size > 0 &&
+                  Array.from(groupMember).map((item) => (
+                    <SelectedMember
+                      handleRemoveMember={() => handleRemoveMember(item)}
+                      member={item}
+                    />
+                  ))}
+              </div>
+              <input
+                className="outline-none border-b border-[#8888] p-2 w-[93%]"
+                type="text"
+                onChange={(e) => {
+                  handleSearch(e.target.value);
+                  setQuery(e.target.value);
+                }}
+                placeholder="Search user"
+                value={query}
+              />
+            </div>
+            <div className=" bg-white overflow-y-scroll h-[50vh]">
+              {query &&
+                [1, 1, 1, 1, 1, 1, 1, 1].map((item) => (
+                  <div
+                    onClick={() => {
+                      groupMember.add(item);
+                      setGroupMember(groupMember);
+                      setQuery("");
+                    }}
+                    key={item?.id}
+                  >
+                    {" "}
+                    <hr /> <ChatCard />
+                  </div>
                 ))}
             </div>
-            <input
-              className="outline-none border-b border-[#8888] p-2 w-[93%]"
-              type="text"
-              onChange={(e) => {
-                handleSearch(e.target.value);
-                setQuery(e.target.value);
+          </div>
+          <div className="absolute bottom-0 py-10 w-[30%] bg-transparent flex items-center justify-center">
+            <div
+              className="bg-[#3e7bd2] rounded-full p-4 cursor-pointer"
+              onClick={() => {
+                setNewGroup(true);
               }}
-              placeholder="Search user"
-              value={query}
-            />
+            >
+              <BsArrowRight className="text-white font-bold text-3xl" />
+            </div>
           </div>
-          <div className="bg-white overflow-y-scroll h-[50.2vh]">
-            {query &&
-              [1, 1, 1].map((item) => (
-                <div
-                  onClick={() => {
-                    groupMember.add(item);
-                    setGroupMember(groupMember);
-                    setQuery("");
-                  }}
-                  key={item?.id}
-                >
-                  {" "}
-                  <hr /> <ChatCard />
-                </div>
-              ))}
-          </div>
-          <div className="bottom-10 py-10 bg-slate-200 flex items-center justify-center">
-          <div className="bg-[#3e7bd2] rounded-full p-4 cursor-pointer" onClick={()=>{
-            setNewGroup(true)
-          }}>
-            <BsArrowRight className="text-white font-bold text-3xl"/>
-          </div>
-
-          </div>
-
-        </div>
+        </>
       )}
-      {newGroup&&<NewGroup/>}
+      {newGroup && <NewGroup />}
     </div>
   );
 };
--- Frontend/src/components/Group/NewGroup.jsx
+++ Frontend/src/components/Group/NewGroup.jsx
@@ -7,45 +7,51 @@ const NewGroup = () => {
   const [groupName, setGroupName] = useState();
   return (
     <div className="w-full h-full ">
-      <div className="flex items-center space-x-10 bg-[#3e7bd2] pt-16 px-10 pb-5 rounded-lg">
-        <BsArrowLeft className="cursor-pointer text-2xl text-white   font-bold" />
-        <p className="text-xl text-white font-semibold">New Group</p>
-      </div>
-
-      <div className="flex flex-col justify-center items-center my-12 ">
-        <label htmlFor="imgInput" className="">
-          <img className="rounded-full w-[15vw] h-[15vw] cursor-pointer" src="luffy.jpeg" alt="" />
-          {isImageUploading && (
-            <CircularProgress className="absolute top-[5rem] left-[6rem]" />
-          )}
-        </label>
-        <input
-          type="file"
-          id="imgInput"
-          className="hidden"
-          onChange={() => console.log("Img Upolode")}
-          value={""}
-        />
-      </div>
-      <div className="w-full flex justify-center items-center py-2 px-5 ">
-        <input
-          className="w-full outline-none border-b-2 border-blue-600 px-2 bg-transparent"
-          placeholder="Group Subject "
-          value={groupName}
-          type="text"
-          onChange={(e) => setGroupName(e.target.value)}
-        />
-      </div>
-
-      {groupName && (
-        <div className="py-10 bg-slate-100 flex items-center justify-center">
-          <Button>
-            <div className="bg-[#3e7bd2] rounded-full p-4 ">
-              <BsCheck2 className="text-white font-bold text-3xl" />
-            </div>
-          </Button>
+      <>
+        <div className="flex items-center space-x-10 bg-[#3e7bd2] pt-16 px-10 pb-5 rounded-lg relative">
+          <BsArrowLeft className="cursor-pointer text-2xl text-white font-bold" />
+          <p className="text-xl text-white font-semibold">New Group</p>
         </div>
-      )}
+
+        <div className="flex flex-col justify-center items-center my-12 ">
+          <label htmlFor="imgInput" className="">
+            <img
+              className="rounded-full w-[15vw] h-[15vw] cursor-pointer"
+              src="luffy.jpeg"
+              alt=""
+            />
+            {isImageUploading && (
+              <CircularProgress className="absolute top-[5rem] left-[6rem]" />
+            )}
+          </label>
+          <input
+            type="file"
+            id="imgInput"
+            className="hidden"
+            onChange={() => console.log("Img Upolode")}
+            value={""}
+          />
+        </div>
+        <div className="w-full flex justify-center items-center py-2 px-5 ">
+          <input
+            className="w-full outline-none border-b-2 border-blue-600 px-2 bg-transparent"
+            placeholder="Group Subject "
+            value={groupName}
+            type="text"
+            onChange={(e) => setGroupName(e.target.value)}
+          />
+        </div>
+
+        {groupName && (
+          <div className="py-10 absolute bottom-0 w-[30%] bg-transparent flex items-center justify-center">
+            <Button>
+              <div className="bg-[#3e7bd2] rounded-full p-4 ">
+                <BsCheck2 className="text-white font-bold text-3xl" />
+              </div>
+            </Button>
+          </div>
+        )}
+      </>
     </div>
   );
 };
