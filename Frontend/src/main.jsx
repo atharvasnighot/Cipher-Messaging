2fb0a79faead610c7463508337e5de827cbc0d4d
Kamesh <kameshrg275@gmail.com>
2023-12-03 13:01:25 +0530

    Added redux library

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import './index.css'
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>  
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
