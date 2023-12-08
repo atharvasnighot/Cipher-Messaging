57d6d3534c3ba4e1e44a1510f914941f0b33bea9
Kamesh <kameshrg275@gmail.com>
2023-12-08 20:34:44 +0530

    Added Redux for Chat and Messages

---
+++ Frontend/src/Redux/Chat/Action.js
@@ -0,0 +1,58 @@
+import { BASE_API_URL } from "../../Config/api";
+import { CREATE_CHAT, CREATE_GROUP, GET_USERS_CHAT } from "./ActionType";
+
+export const createChat = (chatData) => async (dispatch) => {
+  try {
+    const res = await fetch(`${BASE_API_URL}/api/chats/single`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${chatData.token}`,
+      },
+      body: JSON.stringify(chatData.data),
+    });
+
+    const data = await res.json();
+    console.log("create chat: ", data);
+    dispatch({ type: CREATE_CHAT, payload: data });
+  } catch (error) {
+    console.log("catch error", error);
+  }
+};
+
+export const createGroupChat = (chatData) => async (dispatch) => {
+  try {
+    const res = await fetch(`${BASE_API_URL}/api/chats/group`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${chatData.token}`,
+      },
+      body: JSON.stringify(chatData.data),
+    });
+
+    const data = await res.json();
+    console.log("create chat: ", data);
+    dispatch({ type: CREATE_GROUP, payload: data });
+  } catch (error) {
+    console.log("catch error", error);
+  }
+};
+
+export const geyUsersChat = (chatData) => async (dispatch) => {
+  try {
+    const res = await fetch(`${BASE_API_URL}/api/chats/user`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${chatData.token}`,
+      },
+    });
+
+    const data = await res.json();
+    console.log("create chat: ", data);
+    dispatch({ type: GET_USERS_CHAT, payload: data });
+  } catch (error) {
+    console.log("catch error", error);
+  }
+};
--- Frontend/src/Redux/store.js
+++ Frontend/src/Redux/store.js
@@ -1,8 +1,12 @@
-import { applyMiddleware, combineReducers, legacy_createStore } from 'redux';
-import thunk from 'redux-thunk'
-import { authReducer } from './Auth/Reducer';
+import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
+import thunk from "redux-thunk";
+import { authReducer } from "./Auth/Reducer";
+import { chatReducer } from "./Chat/Reducer";
+import { messageReducer } from "./Message/Reducer";
 const rootReducer = combineReducers({
-    auth:authReducer,
-})
+  auth: authReducer,
+  chat: chatReducer,
+  message: messageReducer,
+});
 
- export const store = legacy_createStore(rootReducer,applyMiddleware(thunk))
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
